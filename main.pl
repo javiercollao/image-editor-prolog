4df797bbccaa75ad25f443578421c84a65c2ef49
Javier Collao <javier.collao@usach.cl>
2022-10-17 23:37:23 -0300

    se agrega imageIsBitmap

--- main.pl
+++ main.pl
@@ -1,4 +1,4 @@
-% --------------------------------------------------------------------------
+% ======================================================
 %  @title      TDA lista
 %  @author     Javier Collao
 %
@@ -12,7 +12,7 @@
  
 
 
-% --------------------------------------------------------------------------
+% ======================================================
 %  @title      TDA pixbit-d
 %  @author     Javier Collao
 %
@@ -23,18 +23,21 @@
 %  Dominios
 %  PosX    : Integer
 %  PosY    : Integer 
-%  Bit     : Integer [0|1]
+%  Bit     : Integer [0 ; 1]
 %  Depth   : Integer
-
+%  PixBit  : List
 %  Predicados: 
-%   pixbit(PosX, PosY, Bit, Depth, PixbitList) aridad = 4
+%  pixbit(PosX, PosY, Bit, Depth, PixbitList) aridad = 5
 %  Metas Primarias: pixbit
 %  Metas Secundarias: NA
 %  Clausulas
 
 pixbit(PosX, PosY, Bit, Depth, [PosX, PosY, Bit, Depth]).
 
-% --------------------------------------------------------------------------
+
+
+
+% ======================================================
 %  @title      TDA pixhex-d
 %  @author     Javier Collao
 %
@@ -43,18 +46,27 @@ pixbit(PosX, PosY, Bit, Depth, [PosX, PosY, Bit, Depth]).
 %
 
 %  Dominios
-%  IdSimbolo    : Integer
-%  Simbolo      : String 
+%  PosX    : Integer
+%  PosY    : Integer 
+%  Hex     : String
+%  Depth   : Integer
+%  Pixhex  : List
 %  Predicados
-%  Metas Primarias
-%  Metas Secundarias 
-
+%  pixhex(PosX, PosY, Hex, Depth, Pixhex) aridad = 5
+%  Metas Primarias: pixhex
+%  Metas Secundarias: NA
+%  Clausulas
 
 pixhex(PosX, PosY, Hex, Depth, [PosX, PosY, Hex, Depth]).
 
-%  Clausulas
 
-% --------------------------------------------------------------------------
+
+
+
+
+
+
+% ======================================================
 %  @title      TDA pixrgb-d
 %  @author     Javier Collao
 %
@@ -63,9 +75,15 @@ pixhex(PosX, PosY, Hex, Depth, [PosX, PosY, Hex, Depth]).
 %
 
 %  Dominios
-%  IdSimbolo    : Integer
-%  Simbolo      : String 
+%  PosX    : Integer
+%  PosY    : Integer 
+%  R       : Integer (0 <= R <= 255)
+%  G       : Integer (0 <= G <= 255)
+%  B       : Integer (0 <= B <= 255)
+%  Depth   : Integer
+%  PixRgb  : List
 %  Predicados
+%  pixrgb(PosX, PosY, R, G, B, Depth, PixRgb) aridad = 7
 %  Metas Primarias 
 %  Metas Secundarias  
 %  Clausulas
@@ -73,7 +91,12 @@ pixhex(PosX, PosY, Hex, Depth, [PosX, PosY, Hex, Depth]).
 pixrgb(PosX, PosY, R, G, B, Depth, [PosX, PosY, R, G, B, Depth]).
 
 
-% --------------------------------------------------------------------------
+
+
+
+
+
+% ======================================================
 %  @title      TDA image
 %  @author     Javier Collao
 %
@@ -84,12 +107,64 @@ pixrgb(PosX, PosY, R, G, B, Depth, [PosX, PosY, R, G, B, Depth]).
 %
 
 %  Dominios
-%  IdSimbolo    : Integer
-%  Simbolo      : String 
+%  Width    : Integer
+%  Height   : Integer
+%  Pixels   : Lists
+%  image    : List
 %  Predicados
-%  Metas Primarias
-%  getSimbolo
-%  Metas Secundarias 
+%  image(Width, Height, Pixels, Image) aridad = 4
+%  Metas Primarias: image
+%  Metas Secundarias: NA
 %  Clausulas
 
-image(Width, Height, Pixels, [Width, Height, Pixels])
+image(Width, Height, Pixels, [Width, Height, Pixels]).
+
+
+
+% imageIsBitmap
+
+pixelIsPixbit(Pixel):-
+    pixbit(_,_,Bit,_,Pixel),
+    (Bit == 0 ; Bit == 1).
+
+pixelsAreBitmap([Pixel | Pixels]):-
+    pixelIsPixbit(Pixel),
+    pixelsAreBitmap(Pixels).
+
+imageIsBitmap(Image):-
+    image(_,_,Pixels,Image),
+    pixelsAreBitmap(Pixels).
+
+% imageIsPixmap
+% imageIsHexmap
+% imageIsCompressed
+% imageFlipH
+% imageFlipV
+% imageCrop
+% imageRGBToHex
+% imageToHistogram
+% imageRotate90
+% imageCompress
+% imageChangePixel
+% imageInvertColorRGB
+% imageToString
+% imageDepthLayers
+% imageDecompress
+
+
+
+
+
+
+
+
+% Scripts de prueba
+% ======================================================
+%                       image
+% ======================================================
+
+% pixbit(0,0,1,10,P1),
+% pixbit(0,1,0,20,P2),
+% pixbit(1,0,0,30,P3),
+% pixbit(1,1,1,10,P4),
+% image(2,2,[P1,P2,P3,P4],Img1).
